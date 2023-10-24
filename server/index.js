d900d4fef55ca517470d3221c6193d52f2578a92
romeonatchkebia <121379511+romeonatchkebia@users.noreply.github.com>
2023-10-24 19:26:06 +0400

    userSchema and registration functionality

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -40,8 +41,11 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+
 // MONGOOSE SETUP
-const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 5001;
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
