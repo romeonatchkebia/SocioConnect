0523f9026775f901931cf5c171bdaab583197535
romeonatchkebia <121379511+romeonatchkebia@users.noreply.github.com>
2023-10-25 12:59:30 +0400

    Post routs setup

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
+import { createPost } from "./controllers/posts.js";
 import { register } from "./controllers/auth.js";
 import { verifyToken } from "./middleware/auth.js";
 
